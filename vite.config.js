0684f2aa770583d9a241f7a8e265943cfa3ea6cd
Fredy Funes <136938441+FredyFuness@users.noreply.github.com>
2026-04-10 09:22:08 -0600

    Change base path in vite.config.js
    Update base path in Vite configuration to match repository name.

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
+// ... otros imports si tienes (como react)
 
 export default defineConfig({
-  base: './',
+  base: '/price-pulse-sv/', // ESTA ES LA CLAVE: debe coincidir con el nombre de tu repositorio
+  // ... el resto de tu configuración
 })
